4fa3417c43db9147a22bafad365e6fdd79dfc5df
marstonlm <marston.luann@gmail.com>
2021-05-08 09:12:24 -0700

    Commin on 'main'

--- buildYourOwnCheatSheet/index.php
+++ buildYourOwnCheatSheet/index.php
@@ -1,38 +1,7 @@
 <?php include "includes/header.php";?>
 
-        <table class="table table-striped">
-            <thead class="thead-dark">
-              <tr>
-                <th scope="col">Tag</th>
-                <th scope="col">Name</th>
-                <th scope="col">Description</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td><span class="code">< table ></span> </td>
-                <td>Table</td>
-                <td>The wrapper element for all HTML tables</td>
-              </tr>
-              <tr>
-                <td> < thead > </td>
-                <td>Table Head</td>
-                <td>The set of rows defining the column headers in a table</td>
-              </tr>
-              <tr>
-                <td> < tbody > </td>
-                <td>Table Body</td>
-                <td>The set of rows containing actual table data</td>
-              </tr>
-              <tr>
-                <td> < tr > </td>
-                <td>Table Row</td>
-                <td>The table row container</td>
-              </tr>
-
-            </tbody>
-          </table>
-        
-        
-               
+<p>HTML Table Reference</p>
+<p>CSS Reference</p>
+<p>Bootstrap Reference</p>        
+         
 <?php include "includes/footer.php";?>
